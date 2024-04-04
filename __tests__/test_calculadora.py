a011bcf9d6dad612d850cf7efcb953a34f550f59
RobertaCris <robertacgsilva@hotmail.com>
2024-04-03 21:06:41 -0300

    Criamos as 4 operações aritmeticas basicas e um teste de unidade para a soma

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -10,7 +10,7 @@ def test_somar_dois_numeros():
 
     # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
     
-    # Prepara / Configura
+    # Prepara (fornecer dados) / Configura
     # Dados entrada e saída
     num1 = 5
     num2 = 7
@@ -21,3 +21,68 @@ def test_somar_dois_numeros():
 
     # Assert / Valida
     assert resultado_esperado == resultado_obtido
+
+def test_subtrair_dois_numeros():
+
+    # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
+    
+    # Prepara (fornecer dados) / Configura
+    # Dados entrada e saída
+    num1 = 7
+    num2 = 5
+    resultado_esperado = 2
+
+    # Act / Executa
+    resultado_obtido = subtrair_dois_numeros(num1, num2)
+
+    # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
+def test_multiplicar_dois_numeros():
+
+    # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
+    
+    # Prepara (fornecer dados) / Configura
+    # Dados entrada e saída
+    num1 = 7
+    num2 = 5
+    resultado_esperado = 35
+
+    # Act / Executa
+    resultado_obtido = multiplicar_dois_numeros(num1, num2)
+
+    # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
+def test_dividir_dois_numeros():
+
+    # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
+    
+    # Prepara (fornecer dados) / Configura
+    # Dados entrada e saída
+    num1 = 10
+    num2 = 5
+    resultado_esperado = 2
+
+    # Act / Executa
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
+    # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
+def test_dividir_por_zero():
+
+    # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
+    
+    # Prepara (fornecer dados) / Configura
+    # Dados entrada e saída
+    num1 = 64
+    num2 = 0
+    resultado_esperado = 'Não é possível dividir por zero'
+
+    # Act / Executa
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
+    # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
